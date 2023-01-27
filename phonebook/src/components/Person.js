ca048e59c187d21f14ac80fbb637eed04f70f851
Eemi Kärkkäinen <eemi.karkkainen@aalto.fi>
2023-01-27 14:52:14 +0200

    testing

--- phonebook/src/components/Person.js
+++ phonebook/src/components/Person.js
@@ -4,4 +4,26 @@ const Person = ({ person }) => {
     )
 }
 
-export default Person
+const PersonForm = ({ persons, handlers }) => {
+    return (
+        <form onSubmit={addPerson}>
+            <div>name:
+                <input
+                    value={newName}
+                    onChange={handlers[0]}
+                />
+            </div>
+            <div>number:
+                <input
+                    value={newNumber}
+                    onChange={handlers[0]}
+                />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+export default { Person, PersonForm }
