8dcee5404978246d712fdb8009b1a20804fef2a4
Eemi Kärkkäinen <eemi.karkkainen@aalto.fi>
2023-01-30 12:10:20 +0200

    test

--- test/src/App.js
+++ test/src/App.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  useEffect(() => {
+  const hook = () => {
     console.log('effect')
     axios
       .get('http://localhost:3001/notes')
@@ -15,7 +15,10 @@ const App = (props) => {
         console.log('promise fulfilled')
         setNotes(response.data)
       })
-  }, [])
+  }
+
+  useEffect(hook, [])
+
   console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
