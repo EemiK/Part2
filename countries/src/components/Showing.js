644ba42b8eba444bf031d49eee158dadce1f9026
Eemi Kärkkäinen <eemi.karkkainen@aalto.fi>
2023-02-06 15:37:07 +0200

    new files

---
+++ countries/src/components/Showing.js
@@ -0,0 +1,35 @@
+const Element = ({ value }) => {
+    return (
+        <li>
+            {value}
+        </li>
+    )
+}
+
+const Showing = ({ country }) => {
+    return (
+        <div>
+            <h1>{country.name.common}</h1>
+            <div>capital {country.capital}</div>
+            <div>area {country.area}</div>
+            <h3>
+                <strong>languages:</strong>
+            </h3>
+            <ul>
+                {Object.values(country.languages)
+                    .map(l =>
+                        <Element
+                            value={l}
+                            key={Object.values(country.languages).indexOf(l)}
+                        />
+                    )
+                }
+            </ul>
+            <div>
+                <img src={country.flags.png} />
+            </div>
+        </div>
+    )
+}
+
+export default Showing
