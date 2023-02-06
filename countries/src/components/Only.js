d761d298963f091e31661a24bc6fc8803aec5c21
Eemi Kärkkäinen <eemi.karkkainen@aalto.fi>
2023-02-06 13:10:47 +0200

    test

--- countries/src/components/Only.js
+++ countries/src/components/Only.js
@@ -4,15 +4,12 @@ const Only = ({ country }) => {
             <h1>{country.name.common}</h1>
             <div>capital {country.capital}</div>
             <div>area {country.area}</div>
-            <strong>languages:</strong>
-            <ul>
-                {
-                    country
-                        .languages
-                        .map((t, l) => <li>{l}</li>)
-                }
-            </ul>
-            <img src={country.flag.png} />
+            <div>
+                <strong>languages:</strong>
+            </div>
+            <div>
+                <img src={country.flag.png} />
+            </div>
         </div>
     )
 }
